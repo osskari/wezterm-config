9f41220855082dd4246db41babad5e09d84f46fb
Oskar J. Sandholt <oskarjorgen@gmail.com>
2025-02-25 19:00:40 +0000

    Windows goes direct to wsl

--- config/init.lua
+++ config/init.lua
@@ -5,12 +5,20 @@ return {
   build_config = function()
     local config = wezterm.config_builder()
 
+    if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+      config.default_domain = 'WSL:Debian'
+    end
+
     require('config/style').setup(config, {
       colorscheme = 'OceanicMaterial',
       font = 'FiraCode Nerd Font Mono'
     })
 
-    config.leader = { key = 'b', mods = helpers.platform_mod(), timeout_milliseconds = 2000 }
+    config.leader = {
+      key = 'b',
+      mods = helpers.platform_mod(),
+      timeout_milliseconds = 2000
+    }
 
     config.disable_default_key_bindings = true
     config.keys = require 'config/keymap'
